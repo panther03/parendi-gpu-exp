0212f0d057a72bd5261910bd79e40c18e06375cd
Julien de Castelnau <tripod320@gmail.com>
2025-09-28 12:20:40 +0200

    xorsync benchmark with equal total work

--- tCommTest/runBenchmarks.py
+++ tCommTest/runBenchmarks.py
@@ -1,8 +1,6 @@
 import subprocess
 
 commbytes = [8,16,32,64,128,256]
-#threadcnts = [64,256,512,1024,2048,4096,8192,16384,32768]
-#threadcnts = [64,256,512,1024,2048,4096]
 threadcnts = [64] + [x*256 for x in range(1,17)]
 
 '''
